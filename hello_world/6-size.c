74d545df6512be4676bad74612588f490c0ff607
alyssabmcnatt <6654@holbertonstudents.com>
2023-05-28 22:08:13 -0700

    idc

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -main function
+ * Return: -returns 0
+ */
+
+
+int main()
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+	return (0);
+}
