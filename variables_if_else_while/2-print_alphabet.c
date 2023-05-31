6e18caca61b2fdcb28e3cba4a5c127ca905be19b
alyssabmcnatt <6654@holbertonstudents.com>
2023-05-30 17:50:18 -0700

    idc

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ * Return: Alyssa (Success)
+ */
+
+
+int main(void)
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	purchar(c);
+	putchar('\n');
+	return (0);
