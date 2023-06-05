cedc1221498ea9d6fe6a2f15a4fec073086daa02
alyssabmcnatt <6654@holbertonstudents.com>
2023-06-05 09:50:46 -0700

    idc

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Print the numbers from 0 to 9
- * Return: The numbers from 0 to 9
+ * print_numbers - Print the numbers since 0 up to 9
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
