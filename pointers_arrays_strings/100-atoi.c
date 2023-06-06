e5116f365fb062696d22b75fb9d90377aa1b7f0b
alyssabmcnatt <6654@holbertonstudents.com>
2023-06-05 21:50:02 -0700

    idc

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _atoi - Convert a string to an integer
+ * _atoi - Convert a string to an integer.
  * @s: The pointer to convert
- * Returm: A integer
+ * Return: A integer
  */
 
 int _atoi(char *s)
