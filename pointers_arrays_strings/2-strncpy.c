4fc1cb8dae4527807a0580dc3b557173e4b7aa3a
alyssabmcnatt <6654@holbertonstudents.com>
2023-06-12 12:11:12 -0700

    idc

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * _strncpy - Copies at most an inputted number
  * of bytes from string src into dest.
  * @dest: The buffer storing the string copy.
- * @src: The source string. 
+ * @src: The source string.
  * @n: The maximum number of bytes to copied from src.
  * Return: A pointer to the resulting string dest.
  */
