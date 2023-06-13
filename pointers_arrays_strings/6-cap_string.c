7a0563aa1a67d71cff73f9404a7bbfcee06ef8e9
alyssabmcnatt <6654@holbertonstudents.com>
2023-06-12 20:24:50 -0700

    idc

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 	while (!(str[index] >= 'a' && str[index] <= 'z'))
-	inde++;
+	index++;
 	if (str[index - 1] == ' ' ||
 	str[index - 1] == '\t' ||
 	str[index - 1] == '\n' ||
