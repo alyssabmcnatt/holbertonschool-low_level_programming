de23cd8906d2e11cb3e7a37bbace85b8ef4fa7ba
alyssabmcnatt <6654@holbertonstudents.com>
2023-07-04 14:06:36 -0700

    idc

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -20,7 +20,7 @@ struct dog
  * dog_t - typedef for dog structure
  */
 
-typedef(struct dog dog_t);
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
