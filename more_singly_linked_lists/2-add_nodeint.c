c0913569a21bafdda89032fe36632be28b161049
alyssabmcnatt <6654@holbertonstudents.com>
2023-07-18 20:34:40 -0700

    idc

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -6,7 +6,8 @@
  * @head: a pointer to the address of the head
  * of the listint_t list
  * @n: the integer for the new node to contain
- * Return: if the function fails - NULL, otherwise - the address of the new element
+ * Return: if the function fails - NULL
+ * otherwise - the address of the new element
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
