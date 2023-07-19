2b3f0d6b453eead96bdbd6c2a3984ca9836053a2
alyssabmcnatt <6654@holbertonstudents.com>
2023-07-18 20:33:42 -0700

    typo

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -3,11 +3,10 @@
 /**
  * add_nodeint - adds a new node at the beginning
  * of a listint_t list
- * @head: a pointer to the address of the head 
+ * @head: a pointer to the address of the head
  * of the listint_t list
  * @n: the integer for the new node to contain
- * Return: if the function fails - NULL
- * otherwise - the address of the new element
+ * Return: if the function fails - NULL, otherwise - the address of the new element
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
