db75b604771f6060add59001908420524f18d2a1
alyssabmcnatt <6654@holbertonstudents.com>
2023-11-11 21:10:44 -0800

    return description

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,7 +6,7 @@
  * print_list - prints all the elements of a list_t list
  * @h: the list_t list
  *
- * return: the number of nodes in h
+ * return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
