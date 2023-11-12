8d955f07b499d330403803b169203a9d9294609d
alyssabmcnatt <6654@holbertonstudents.com>
2023-11-12 15:40:09 -0800

    idc

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,6 +5,7 @@
  * add_nodeint_end - adds a new node at the end of a list
  * @head: pointer pointing to the head of the list
  * @n: data to be added to the new list
+ * description: adds a new node at the end of a list
  * return: the address of the new element, or NULL if failed
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
@@ -33,7 +34,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		}
 		temp->next = new;
 	}
-	
+
 	return (*head);
 }
 
