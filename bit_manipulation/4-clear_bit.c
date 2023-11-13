15c6eaf3caf333185750237351f60993d232f2ad
alyssabmcnatt <6654@holbertonstudents.com>
2023-11-12 19:49:30 -0800

    idc

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -15,7 +15,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	mask = mask << index;
 	if (index > sizeof(unsigned long int) * 8 || n == NULL)
 		return (-1);
-	if (((*n >> index) & 1) = 1)
+	if (((*n >> index) & 1) == 1)
 		*n = mask ^ *n;
 
 	return (1);
