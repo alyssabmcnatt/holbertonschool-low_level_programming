81def1d3c391d4cad569ed16243672e8f4a11c7b
alyssabmcnatt <6654@holbertonstudents.com>
2023-11-12 19:38:42 -0800

    idc

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - set the value of a bit to 1
+ * @n: pointer to the bit to be manipulated
+ * @index: position fo the bit to be manipulated
+ * return: 1 if success, -1 if fail
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask;
+
+	if (index > sizeof(unsigned int) * 8)
+		return (-1);
+	mask = 1;
+	mask - mask << index;
+	*n = ((*n) | mask);
+	return (1);
+}
