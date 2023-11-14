fbcf84a2089617e4dbeeab62cca6c41e1de2ff0e
alyssabmcnatt <6654@holbertonstudents.com>
2023-11-13 19:03:52 -0800

    idc

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <unisted.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
  * append_text_to_file - appends text at end of file
