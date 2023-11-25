7371c5164689152a45fe619ebf5bcc3df5f72c9d
alyssabmcnatt <6654@holbertonstudents.com>
2023-11-24 20:36:56 -0800

    idc

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_yable_t *ht, const char *key, const char *value);
 
 /**
