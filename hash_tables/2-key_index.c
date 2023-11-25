5e5fdc62b5ffb5184b7ddfd5517bc32816c49f3b
alyssabmcnatt <6654@holbertonstudents.com>
2023-11-24 19:09:16 -0800

    idc

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_table.h"
+
+/**
+ * key_index - get index
+ * @key: get index of the key
+ * Return: index of the key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_table.h"
+
+/**
+ * hash_table_get - retrieves a value associated with key
+ * @ht: pointer to hash table
+ * @key: key to get the value
+ * Return: value associated with element if success, or NULL if fail
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
+}	
